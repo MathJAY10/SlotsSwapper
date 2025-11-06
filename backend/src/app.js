358b59496c6e8532f96805a5f692166795724844
MathJAY10 <unknown9a1@gmail.com>
2025-11-06 18:15:10 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://slots-swapper.vercel.app'
+  ],
+  credentials: true
+}));
 
 setupRoutes(app);
 
